b8b188b67380125ca71705ff3acd45e4e1cc4d6a
Gautam <gautmalhotra@deloitte.com>
2022-11-11 14:14:58 +0530

    Modules imported2

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import {useState} from 'react'
-import {useQuery} from 'react-query'
+import React from 'react';
+import { useState } from "react";
+import { useQuery } from "react-query";
 //Components
-import Drawer from '@material-ui/core/Drawer';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import Grid from '@material-ui/core/Grid';
-import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
-import Badge from '@material-ui/core/Badge';
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
 // Styles
-import { Wrapper } from './App.styles';
+import { Wrapper } from "./App.styles";
 //Types
-export type CartItemType={
-  id:number;
-  category:string;
-  description:string;
-  image:string;
-  price:number;
-  title:string;
-  amount:number;
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
 };
 
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
 
-const getProducts = async():Promise<CartItemType>=>
-  await(await fetch('https://fakestoreapi.com/products')).json();
-
-const App=() => {
-  return (
-    <div className="App">
-      Start
-    </div>
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
   );
-}
+  console.log(data);
+  return <div className="App">Start</div>;
+};
 
 export default App;
