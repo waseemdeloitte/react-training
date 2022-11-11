8ed5b4bbb757d36fac7ba19cae50f7ae79b02937
Gautam <gautmalhotra@deloitte.com>
2022-11-11 17:37:22 +0530

    List of Products on Console

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
 import { useQuery } from "react-query";
 //Components
+import Item from "./Item/Item";
 import Drawer from "@material-ui/core/Drawer";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Grid from "@material-ui/core/Grid";
@@ -9,6 +10,7 @@ import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import Badge from "@material-ui/core/Badge";
 // Styles
 import { Wrapper } from "./App.styles";
+import { CircularProgress } from "@material-ui/core";
 //Types
 export type CartItemType = {
   id: number;
@@ -29,7 +31,26 @@ const App = () => {
     getProducts
   );
   console.log(data);
-  return <div className="App">Start</div>;
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went Wrong</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 };
 
 export default App;
