ba076e3119351111bb430aa39acdc270fcbb9015
Gautam <gautmalhotra@deloitte.com>
2022-11-11 13:59:17 +0530

    Modules imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+import {useState} from 'react'
+import {useQuery} from 'react-query'
+//Components
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+// Styles
+import { Wrapper } from './App.styles';
+//Types
+export type CartItemType={
+  id:number;
+  category:string;
+  description:string;
+  image:string;
+  price:number;
+  title:string;
+  amount:number;
+};
+
+
+const getProducts = async():Promise<CartItemType>=>
+  await(await fetch('https://fakestoreapi.com/products')).json();
 
 const App=() => {
   return (
